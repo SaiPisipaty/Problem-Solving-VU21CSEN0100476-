2f0072c069c998a26d04ed044e7df9af8c908bba
SaiPisipaty <95023511+SaiPisipaty@users.noreply.github.com>
2021-11-29 20:08:02 +0530

    Create Prime Or Composite in C .c

---
+++ Prime Or Composite in C .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int a;
+printf("Check whether the number is prime or composite");
+printf("\n Enter the value of a");
+scanf("%d",&a);
+if(a%2==0)
+{ printf("The number is composite");
+}
+else
+{ printf("The number is prime");
+}
+return 0;
+}
