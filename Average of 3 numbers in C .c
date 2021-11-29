6f0e2749897b840b426733099146e978e767e64d
SaiPisipaty <95023511+SaiPisipaty@users.noreply.github.com>
2021-11-29 20:13:21 +0530

    Create Average of 3 numbers in C .c

---
+++ Average of 3 numbers in C .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+float d;
+printf("Enter the value of a,b and c");
+scanf("%d %d %d",&a,&b,&c);
+d=(a+b+c)/3;
+printf("Average of 3 numbers is %0.2f",d);
+return 0;
+}
