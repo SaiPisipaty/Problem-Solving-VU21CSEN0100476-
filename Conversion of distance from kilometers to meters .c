38f51f3065c16132d8a88e9886492ecdce244289
SaiPisipaty <95023511+SaiPisipaty@users.noreply.github.com>
2021-11-29 21:51:35 +0530

    Create Conversion of distance from kilometers to meters .c

---
+++ Conversion of distance from kilometers to meters .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+float km,m;
+printf("Enter distance in kilometers : ");
+scanf("%f",&km);
+
+m=km*1000;
+printf("Kilometers in meters is = %.2f",m);
+
+return 0;
