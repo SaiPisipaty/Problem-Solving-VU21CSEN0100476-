01b3f8fd03610ebc035a348ae74ad6106d8fae2d
SaiPisipaty <95023511+SaiPisipaty@users.noreply.github.com>
2021-11-29 12:04:09 +0530

    Create Loop for odd and even functions in c .c

---
+++ Loop for odd and even functions in c .c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+ int a,b;
+ printf("Enter the  value of a : ");
+ scanf("%d",&a);
+ for(a;a<=25;a+=2)
+ 
+ {
+     printf("\n  %d",a);
+ }
+ printf("\n enter the  value of b : ");
+ scanf("%d",&b);
+   for(b;b<=27 ;b+=2)
+   {
+   	 printf("\n %d",b);
+   }
+  return 0;
+}
