1643a7e4c7db8376635920a4f3ad7b4b511bb116
SaiPisipaty <95023511+SaiPisipaty@users.noreply.github.com>
2021-11-29 22:13:23 +0530

    Create Finding area of a square in C .c

---
+++ Finding area of a square in C .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{ 
+float a, A;
+ printf("Enter the length of the side of a square=");
+ scanf("%f",&a);
+ A=a*a;
+ printf("The area of the square is %f",A);
+ return 0;
+        }
