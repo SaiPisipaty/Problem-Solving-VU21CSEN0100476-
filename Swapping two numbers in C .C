d20912fa0087ae3949bc6c2f032955abdab7cca5
SaiPisipaty <95023511+SaiPisipaty@users.noreply.github.com>
2021-12-20 11:45:45 +0530

    Create Swapping two numbers in C .C

---
+++ Swapping two numbers in C .C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int a,b;
+printf("Enter the value of a before swapping:");
+scanf("%d",&a);
+printf("Enter the value of b before swapping:");
+scanf("%d",&b);
+printf("The value of a after swapping is: %d",b);
+printf("\nThe value of b after swapping is: %d",a);
+return 0;
+}
