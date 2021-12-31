31d9f7efa3eb8d3b706657c0107e684a8ef04799
SaiPisipaty <95023511+SaiPisipaty@users.noreply.github.com>
2021-12-31 18:33:37 +0530

    Create Factorial of a number .C

---
+++ Factorial of a number .C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{int a,b,i;
+ b=1;
+ printf("Enter the number you want to find the factorial of:");
+ scanf("%d",&a);
+ for(i=1;i<=a;i++)
+ {b=b*i
+  }
+ printf("The factorial of %d is",b);
+ return 0;
+}
